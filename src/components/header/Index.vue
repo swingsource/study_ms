8cf007755f348e9a3292028147151e187146ebec
郝鹏 <swingsource@163.com>
2021-03-08 17:29:06 +0800

    增加 header组件

---
+++ src/components/header/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <div class="left">
+      <img src="./logo.png" alt="" />
+      <span class="title">MS蓝狐</span>
+    </div>
+    <div class="right">
+      <div class="avatar"></div>
+      <div class="username">我在等风</div>
+      <div class="switch-theme"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+.header
+  margin 0 20px
+  height 100%
+  display flex
+  flex-flow row nowrap
+  justify-content space-between
+  align-items center
+  .left
+    display flex
+    align-items center
+    img
+      width 36px
+      height 36px
+    .title
+      margin-left 12px
+      font-size 20px
+  .right
+    display flex
+    align-items center
+    .avatar
+      width 24px
+      height 24px
+      border-radius 50%
+      background green
+    .username, .switch-theme
+      margin-left 20px
+    .username
+      font-size 16px
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Tradition from '@/views/layout/Tradition'
+import Card from '@/views/layout/Card'
 
 Vue.use(VueRouter)
 
@@ -10,6 +12,16 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/tra',
+    name: 'Tradition',
+    component: Tradition
+  },
+  {
+    path: '/card',
+    name: 'Card',
+    component: Card
+  }
 ]
 
 const router = new VueRouter({
