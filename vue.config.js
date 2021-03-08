449c909ee2083283d3e981c61711d4f8fbf64a15
郝鹏 <swingsource@163.com>
2021-03-08 16:05:09 +0800

    项目初始化。删除不必要的文件

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const resolve = (fileName) => {
+  return path.join(__dirname, filename)
+}
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('cps', resolve('src/components'))
+      .set('core', resolve('src/core'))
+  }
+}
