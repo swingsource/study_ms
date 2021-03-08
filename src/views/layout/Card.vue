b4d07a452faddf29762024d311587b168a666c15
郝鹏 <swingsource@163.com>
2021-03-08 17:28:41 +0800

    增加  两种入口布局文件

---
+++ src/views/layout/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="header-wrapper">
+      <i-header></i-header>
+    </div>
+    <div class="main-wrapper">内容区域</div>
+  </div>
+</template>
+
+<script>
+import IHeader from 'cps/header/Index'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    IHeader,
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+.card
+  height 100%
+  .header-wrapper
+    height 60px
+  .main-wrapper
+    height calc(100% - 60px)
+    background #eee
+</style>
---
+++ src/views/layout/Tradition.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tradition">
+    <div class="header-wrapper">
+      <i-header></i-header>
+    </div>
+    <div class="main-wrapper">
+      <div class="left">我是左侧</div>
+      <div class="right">我是右侧，内容显示区域</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IHeader from 'cps/header/Index'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    IHeader,
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+.tradition
+  height 100%
+  .header-wrapper
+    height 60px
+  .main-wrapper
+    height calc(100% - 60px)
+    display flex
+    flex-flow row nowrap
+    background #cccccc
+    .left
+      width 240px
+    .right
+      flex 1
+      background green
+</style>
