459a94520f52189ebff4316421f5bf5c73221f4c
swingsource <swingsource@163.com>
2021-03-08 23:23:36 +0800

    修改字体大小，添加了switch按钮

--- src/components/header/Index.vue
+++ src/components/header/Index.vue
@@ -7,10 +7,26 @@
     <div class="right">
       <div class="avatar"></div>
       <div class="username">我在等风</div>
-      <div class="switch-theme"></div>
+      <div class="switch-theme">
+        <el-switch
+            v-model="themeType"
+            active-color="#000"
+            inactive-color="#eee">
+        </el-switch>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      themeType: false
+    }
+  },
+}
+</script>
 
 <style scoped lang="stylus">
 .header
@@ -38,7 +54,7 @@
       border-radius 50%
       background green
     .username, .switch-theme
-      margin-left 20px
+      margin-left 10px
     .username
-      font-size 16px
+      font-size 14px
 </style>
