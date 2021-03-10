d040d621540b4b0b369d81697390c0fe0d07683f
郝鹏 <swingsource@163.com>
2021-03-10 17:32:38 +0800

    增加 修改主题部分逻辑

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
   </div>
 </template>
 
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  mounted() {
+    document.documentElement.setAttribute('theme', this.theme)
+  },
+  computed: {
+    ...mapState(['theme']),
+  },
+}
+</script>
+
 <style lang="stylus">
 @import '~@/assets/theme/index.css'
 #app
