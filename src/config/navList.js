ca1c16e93bd91f982c8fac3820a3f08c96d83c35
郝鹏 <swingsource@163.com>
2021-03-12 13:55:35 +0800

    增加 左侧菜单栏数据文件

---
+++ src/config/navList.js
@@ -0,0 +1,32 @@
+export const navList = [
+  {
+    name: '首页',
+    icon: 'el-icon-house',
+    path: 'home',
+  },
+  {
+    name: '用户管理',
+    icon: 'el-icon-user',
+    path: 'user',
+  },
+  {
+    name: '书籍管理',
+    icon: 'el-icon-reading',
+    path: 'book',
+  },
+  {
+    name: '留言管理',
+    icon: 'el-icon-chat-line-square',
+    path: 'message',
+  },
+  {
+    name: '推荐审核',
+    icon: 'el-icon-view',
+    path: 'approval',
+  },
+  {
+    name: '前台监测',
+    icon: 'el-icon-data-line',
+    path: 'monitor',
+  }
+]
