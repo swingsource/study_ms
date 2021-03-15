cc8e19d6edee18014ca704368ffd3cde089b3ed8
郝鹏 <swingsource@163.com>
2021-03-15 17:32:50 +0800

    增加 路由添加meta属性

--- src/router/cardRoute.js
+++ src/router/cardRoute.js
@@ -1,36 +1,54 @@
 export default [
   {
     path: '',
-    component: () => import('@/views/layout/CardNav')
+    redirect: 'home'
   },
   {
     path: 'home',
     name: 'Home',
-    component: () => import('@/views/home/Index.vue')
+    meta: {
+      navName: '首页'
+    },
+    component: () => import('@/views/layout/CardNav')
   },
   {
     path: 'user',
     name: 'User',
+    meta: {
+      navName: '用户管理'
+    },
     component: () => import('@/views/user/Index.vue')
   },
   {
     path: 'book',
     name: 'Book',
+    meta: {
+      navName: '书籍管理'
+    },
     component: () => import('@/views/book/Index.vue')
   },
   {
     path: 'message',
     name: 'Message',
+    meta: {
+      navName: '留言管理'
+    },
     component: () => import('@/views/message/Index.vue')
   },
   {
     path: 'approval',
     name: 'Approval',
+    meta: {
+      navName: '推荐审核'
+    },
     component: () => import('@/views/approval/Index.vue')
   },
   {
     path: 'monitor',
     name: 'Monitor',
+    meta: {
+      navName: '前台监测'
+    },
     component: () => import('@/views/monitor/Index.vue')
   },
 ]
--- src/router/traRoute.js
+++ src/router/traRoute.js
@@ -6,31 +6,49 @@ export default [
   {
     path: 'home',
     name: 'Home',
+    meta: {
+      navName: '首页'
+    },
     component: () => import('@/views/home/Index.vue')
   },
   {
     path: 'user',
     name: 'User',
+    meta: {
+      navName: '用户管理'
+    },
     component: () => import('@/views/user/Index.vue')
   },
   {
     path: 'book',
     name: 'Book',
+    meta: {
+      navName: '书籍管理'
+    },
     component: () => import('@/views/book/Index.vue')
   },
   {
     path: 'message',
     name: 'Message',
+    meta: {
+      navName: '留言管理'
+    },
     component: () => import('@/views/message/Index.vue')
   },
   {
     path: 'approval',
     name: 'Approval',
+    meta: {
+      navName: '推荐审核'
+    },
     component: () => import('@/views/approval/Index.vue')
   },
   {
     path: 'monitor',
     name: 'Monitor',
+    meta: {
+      navName: '前台监测'
+    },
     component: () => import('@/views/monitor/Index.vue')
   }
 ]
