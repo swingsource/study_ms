8bcef12d2a25ea051b9277287de311c3625187e0
swingsource <swingsource@163.com>
2021-03-24 15:22:55 +0800

    :sparkles: 添加前端分页方法

--- src/util/utils.js
+++ src/util/utils.js
@@ -6,3 +6,8 @@ export const groupArray = (arr, grounpNum) => {
       return arr.slice(grounpNum * i, grounpNum * (i + 1))
     })
 }
+
+// 前端分页方法
+export const fePagination = (originList, pageSize, pageNum) => {
+    return originList.slice(pageSize * (pageNum - 1), pageSize * pageNum)
+}
