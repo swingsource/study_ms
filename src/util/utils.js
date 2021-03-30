9a0c15df133c3d8044c4d8cd691c065147b3bd63
swingsource <swingsource@163.com>
2021-03-30 09:11:56 +0800

    :sparkles: 添加spider list =》 tree 的方法

--- src/util/utils.js
+++ src/util/utils.js
@@ -11,3 +11,54 @@ export const groupArray = (arr, grounpNum) => {
 export const fePagination = (originList, pageSize, pageNum) => {
     return originList.slice(pageSize * (pageNum - 1), pageSize * pageNum)
 }
+
+// spider result: list => tree
+export const spiderListToTree = (targetList) => {
+    // 根据 chapter_level_two_parent_id 字段将数组转为二维数组
+    const fieldList = []
+    targetList.forEach(item => {
+        fieldList.push(item.chapter_level_two_parent_id)
+    })
+    //// fieldList 去重
+    const fieldListSingle = Array.from(new Set(fieldList))
+    //// 根据 fieldListSingle 中的 chapter_level_two_parent_id 将原数组转成二维
+    /// 这样得到的每个子数组都是同一父节点下的子节点了
+    const targetList2d = []
+    fieldListSingle.forEach(level2Id => {
+        const tempList = []
+        targetList.forEach(item => {
+            if (item.chapter_level_two_parent_id === level2Id) {
+                tempList.push(item)
+            }
+        })
+        targetList2d.push(tempList)
+    })
+
+    // 将二维数组转换为tree
+    const resultList = []
+    targetList2d.forEach(list => {
+        const tempObj = {}
+        // parent node
+        tempObj.parentId = 0
+        tempObj.name = list[0]['chapter_level_one_name']
+        tempObj.parentSortIndex = list[0]['parent_sort_index']
+        tempObj.children = []
+        list.length && list.forEach(item => {
+            const obj = {}
+            obj.sortIndex = item.sort_index
+            obj.name = item.chapter_level_two_name
+            obj.content = item.book_content
+            tempObj.children.push(obj)
+            // 子数组按照sortIndex排序
+            tempObj.children.sort((a, b) => {
+                return a.sortIndex - b.sortIndex
+            })
+        })
+        resultList.push(tempObj)
+        // 数组按照parentSortIndex排序
+        resultList.sort((a, b) => {
+            return a.parentSortIndex - b.parentSortIndex
+        })
+    })
+    return resultList
+}
